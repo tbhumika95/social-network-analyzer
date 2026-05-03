aba879eeffea1c9452f30a80a16bc576706393c4
Bhumika Tiwari <tbhumika95@gmail.com>
2026-05-03 12:27:28 +0530

    Initial Commit : basic graph, user and friendship featureswith Validation

---
+++ README.md
@@ -0,0 +1,24 @@
+SOCIAL NETWORK ANALYZER(DSA Project)
+
+Description :
+This Project simulates a social network using Graph data structure.
+Users are represented as nodes and friendships as edges.
+
+Features :
+1. Add Users
+2. Add Friendships
+3. Display Network
+4. Validation(no duplicate users or friendships)
+
+Data Structures Used :
+1. Graph(Adjacency List)
+2. HashMap(unordered_map)
+
+How to Run :
+1. Compile the code : g++ main.cpp -o app
+2. Run: ./app
+
+Future Improvements 
+1. Friend Recommendation using BFS
+2. Mutual Friends
+3. Shortest path between Users
---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+unordered_map<string , vector<string>>graph;
+
+bool alreadyFriend(string a , string b){
+    for(auto &f : graph[a]){
+        if(f == b)return true;
+    }
+    return false;
+}
+
+void addUser(string user){
+    if(graph.find(user) != graph.end()){
+        cout<<"User Already Exists!!"<<endl;
+        return;
+    }
+    graph[user] = {};
+}
+
+void addFriend(string a , string b){
+    if(graph.find(a) == graph.end() || graph.find(b) == graph.end()){
+        cout<<"User Not Found!!"<<endl;
+        return;
+    }
+    if(alreadyFriend(a,b)){
+        cout<<"Already a Friend!!"<<endl;
+        return;
+    }
+    graph[a].push_back(b);
+    graph[b].push_back(a);
+}
+
+void display(){
+    for(auto &p : graph){
+        cout<<p.first<<"->";
+        for(auto &f : p.second){
+            cout<<f<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    int choice;
+    string u , v;
+
+    while(true){
+        cout<<"\n1. Add User\n2. Add Friend\n3. Display\n4. Exit\n";
+        cin>>choice;
+
+        if(choice == 1){
+            cin>>u;
+            addUser(u);
+        }
+        else if(choice == 2){
+            cin>>u>>v;
+            addFriend(u , v);
+        }
+        else if(choice == 3){
+            display();
+        }
+        else break;
+    }
+}
